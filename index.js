726fec85ddce17d7cca8f65e8973001b96a04b25
Darlen Kitz Suizo <suizoda@oocl.com>
2021-07-22 16:30:34 +0800

    array exercise

--- index.js
+++ index.js
@@ -2,25 +2,84 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+document.write(typeof(a));
+document.write(" " + typeof(b));
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
 
+for (let i = 0; i <a.length; i++){
+  document.write(a[i] * 2 + " ");
+}
+
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
-// case 3 output: 'Red,Green,White,Black'
+// case 3 output: 'Red,Green,White,Black
+
+var colors = ["Red", "Green", "White", "Black"];
+var firstChecker = " ";
+var secondChecker = "+";
+var thirdChecker = ",";
+var colors = ["Red", "Green", "White", "Black"];
+myFunction(colors,firstChecker);
+myFunction(colors,secondChecker);
+myFunction(colors,thirdChecker);
+function myFunction(color,tocheck){
+document.write("'");
+for (let i = 0; i <color.length; i++){
+  document.write(color[i]);
+  checker(i,tocheck);
+}
+document.write("' ");
+}
+
+function checker(count,check){
+if(count <3 ){
+      document.write(check);
+  }
+}
+
+
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
 
+a.sort( function( b , c){
+    if(b > c) return 1;
+    if(b < c) return -1;
+    return 0;
+});
 
+ document.write(a);
+
+
+
+document.write(a.sort())
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var mf = 1;
+var m = 0;
+var item;
+for (var i=0; i<a.length; i++)
+{
+        for (var j=i; j<a.length; j++)
+        {
+                if (a[i] == a[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = a[i];
+                }
+        }
+        m=0;
+}
+document.write(item+" ( " +mf +" times ) ") ;
 //TODO should output: 'a'
